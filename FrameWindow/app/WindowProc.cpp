643a2f29b490802b8e0a691fbe896fd0e90cc083
YOSHIMURA Takanori <github@studiomemoru.com>
2022-05-21 21:52:24 +0900

    Add blank "resource.h" and include it in "configure.hpp".

--- FrameWindow/app/WindowProc.cpp
+++ FrameWindow/app/WindowProc.cpp
@@ -2,8 +2,6 @@
  */
 
 #include "../configure.hpp"
-
-#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
 extern HINSTANCE g_instance;
@@ -15,9 +13,9 @@ LRESULT CALLBACK WMB_WNDPROC(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPar
     case WM_CREATE:
         MoveWindow(hWnd, 100, 100, 320, 240, FALSE);
 		CreateWindow(
-			L"BUTTON", L"Button",
+			L"BUTTON", L"WinMainBoot",
 			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-			4, 4, 100, 50,
+			4, 4, 150, 50,
 			hWnd, nullptr, g_instance, nullptr);
 
         return 0;
--- FrameWindow/configure.hpp
+++ FrameWindow/configure.hpp
@@ -1,10 +1,14 @@
 #pragma once
+/*
+ */
 
 #include <SDKDDKVer.h>
+#include "resources/resource.h"
 
-#define WMB_WINDOW_CLASS_NAME	L"WinMainBoot"
-#define WMB_TITLE	L"Main Window"
 
-//#define WMB_RES_ACCELERATORS	L"WinMainBoot"
+#define WMB_WINDOW_CLASS_NAME		L"WinMainBoot"
+#define WMB_TITLE		L"Main Window"
 
-#define WMB_WNDPROC	MyWindowProc
+//#define WMB_RES_ACCELERATORS		L"WinMainBoot"
+
+#define WMB_WNDPROC		MyWindowProc
